403c74ec6f132799ad2d36df25a6e863ebc32f4a
Anghel Aranyos <anghel.aranyos@fortech.ro>
2018-09-27 14:59:48 +0300

    Stored Cassandra Properties in prop file

--- src/main/java/com/example/springBatch/jobs/CassandraConfiguration.java
+++ src/main/java/com/example/springBatch/jobs/CassandraConfiguration.java
@@ -2,51 +2,52 @@ package com.example.springBatch.jobs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.CassandraTemplate;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Host;
-import com.datastax.driver.core.Metadata;
-import com.datastax.driver.core.Session;
 import com.datastax.driver.core.policies.DowngradingConsistencyRetryPolicy;
 import com.datastax.driver.core.policies.ExponentialReconnectionPolicy;
 import com.datastax.driver.core.policies.ReconnectionPolicy;
 import com.datastax.driver.core.policies.RetryPolicy;
 import com.example.springBatch.model.Person;
+import com.example.springBatch.properties.PropertyKeys;
 
 @Configuration
+@PropertySource(value = {"classpath:application.properties"})
 //@EnableCassandraRepositories
 public class CassandraConfiguration  extends AbstractCassandraConfiguration {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CassandraConfiguration.class);
 
-    private final String LOCAL_HOST = "127.0.0.1";
-
-    private final String KEY_SPACE = "personkeyspace";
+	
+	@Autowired
+	private Environment propertyEnvironment;
+	
 
     @Override
     protected String getKeyspaceName() {
-        return KEY_SPACE;
+        return propertyEnvironment.getProperty(PropertyKeys.CASSANDRA_KEYSPACE);
     }
 
     @Override
     protected String getContactPoints() {
-        return LOCAL_HOST;
+        return propertyEnvironment.getProperty(PropertyKeys.CASSANDRA_CONTACT_POINTS);
     }
 
     @Override
     protected int getPort() {
-        return 9042;
+        return Integer.parseInt(propertyEnvironment.getProperty(PropertyKeys.CASSANDRA_PORT));
     }
 
     @Override
     public SchemaAction getSchemaAction() {
-        return SchemaAction.CREATE_IF_NOT_EXISTS;
+        return SchemaAction.valueOf(propertyEnvironment.getProperty(PropertyKeys.CASSANDRA_SCHEMA_ACTION).toUpperCase());
     }
 
     @Override
---
+++ src/main/java/com/example/springBatch/properties/PropertyKeys.java
@@ -0,0 +1,10 @@
+package com.example.springBatch.properties;
+
+public class PropertyKeys {
+	
+	public static String CASSANDRA_KEYSPACE = "spring.data.cassandra.keyspace-name";
+	public static String CASSANDRA_CONTACT_POINTS = "spring.data.cassandra.contact-points";
+	public static String CASSANDRA_PORT = "spring.data.cassandra.port";
+	public static String CASSANDRA_SCHEMA_ACTION = "spring.data.cassandra.schema-action";
+
+}
